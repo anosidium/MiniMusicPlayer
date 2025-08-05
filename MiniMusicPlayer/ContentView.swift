2b340d06a0950d56a69deae15b8cdf86c79797ae
Ammad <anosidium@hotmail.com>
2025-08-05 14:12:55 +0100

    Initial Commit

---
+++ MiniMusicPlayer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MiniMusicPlayer
+//
+//  Created by Ammad on 05/08/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MiniMusicPlayer/MiniMusicPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniMusicPlayerApp.swift
+//  MiniMusicPlayer
+//
+//  Created by Ammad on 05/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct MiniMusicPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

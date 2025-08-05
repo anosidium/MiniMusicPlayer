f2726f8dc6f5ffec77df8776e0ce019526cf24a7
Ammad <anosidium@hotmail.com>
2025-08-05 15:44:56 +0100

    Create MiniPlayerView.swift file and build the view

---
+++ MiniMusicPlayer/MiniPlayerView.swift
@@ -0,0 +1,48 @@
+//
+//  MiniPlayerView.swift
+//  MiniMusicPlayer
+//
+//  Created by Ammad on 05/08/2025.
+//
+
+import SwiftUI
+
+struct MiniPlayerView: View {
+
+    var body: some View {
+        VStack {
+            HStack {
+                Image(.speakNowAlbum)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 40, height: 40)
+                    .clipShape(.rect(cornerRadius: 8))
+
+                Spacer()
+
+                VStack {
+                    Text("Title")
+                        .font(.caption)
+
+                    Text("Album")
+                        .font(.caption2)
+                }
+
+                Spacer()
+
+                Button("Play", systemImage: "play", action: {})
+                    .labelStyle(.iconOnly)
+                    .frame(width: 40, height: 40)
+            }
+
+            Slider(value: .constant(0.5))
+                .controlSize(.mini)
+        }
+        .padding()
+    }
+
+}
+
+#Preview {
+    MiniPlayerView()
+}

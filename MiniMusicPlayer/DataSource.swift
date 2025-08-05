e0fb19a4f6f258bb61fdfee88401a5a469d65d83
Ammad <anosidium@hotmail.com>
2025-08-05 14:47:12 +0100

    Make a mock data

--- MiniMusicPlayer/DataSource.swift
+++ MiniMusicPlayer/DataSource.swift
@@ -7,6 +7,8 @@
 
 import Foundation
 
+// MARK: - Genre
+
 enum Genre {
     case alternative
     case country
@@ -14,6 +16,19 @@ enum Genre {
     case rock
     case jazz
 }
+
+// MARK: - Track
+
+struct Track {
+    let title: String
+    let duration: Int
+    let artist: String
+    let album: String
+    let number: Int
+    var isFavourited: Bool
+}
+
+// MARK: - Album
 
 struct Album {
     let name: String
@@ -23,12 +38,24 @@ struct Album {
     let tracks: [Track]
 }
 
-struct Track {
-    let title: String
-    let duration: Int
-    let artist: String
-    let album: String
-    let number: Int
-    let discNumber: Int
-    var isFavourited: Bool
+extension Album {
+
+    static let speakNow: Album = {
+        let tracks = [
+            Track(title: "Mine (POP Mix)", duration: 99, artist: "Taylor Swift", album: "Speak Now", number: 1, isFavourited: false),
+            Track(title: "Sparks Fly", duration: 99, artist: "Taylor Swift", album: "Speak Now", number: 2, isFavourited: false),
+            Track(title: "Back To December", duration: 99, artist: "Taylor Swift", album: "Speak Now", number: 3, isFavourited: false),
+        ]
+
+        let album = Album(
+            name: "Speak Now",
+            artist: "Taylor Swift",
+            year: 2010,
+            genre: .pop,
+            tracks: tracks
+        )
+
+        return album
+    }()
+
 }

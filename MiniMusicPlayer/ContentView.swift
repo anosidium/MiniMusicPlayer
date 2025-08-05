a6f47f7f25dd20c5b6584fa15749757399986620
Ammad <anosidium@hotmail.com>
2025-08-05 15:15:31 +0100

    Add an album artwork to the catalogue Build the basic full screen player

--- MiniMusicPlayer/ContentView.swift
+++ MiniMusicPlayer/ContentView.swift
@@ -8,17 +8,89 @@
 import SwiftUI
 
 struct ContentView: View {
+    private let album: Album
+
+    init(album: Album = .speakNow) {
+        self.album = album
+    }
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack {
+                Button("Close", systemImage: "chevron.up", action: {})
+                    .labelStyle(.iconOnly)
+
+                Spacer()
+
+                Text("Now Playing")
+                    .bold()
+
+                Spacer()
+
+                Button("Close", systemImage: "ellipsis.circle", action: {})
+                    .labelStyle(.iconOnly)
+            }
+
+            Image(.speakNowAlbum)
+                .resizable()
+                .scaledToFit()
+
+            Text(album.name)
+                .font(.title)
+                .bold()
+
+            Text(album.artist)
+
+            HStack {
+                Button("Favourite", systemImage: "heart", action: {})
+                    .symbolVariant(.fill)
+                    .labelStyle(.iconOnly)
+                
+                Spacer()
+                
+                Button("Skip", systemImage: "square.and.arrow.up", action: {})
+                    .labelStyle(.iconOnly)
+            }
+
+            Slider(value: .constant(0.75)) {
+                Text("Label")
+            } minimumValueLabel: {
+                Text("Min")
+            } maximumValueLabel: {
+                Text("Max")
+            }
+
+            HStack {
+                Button("Shuffle", systemImage: "shuffle", action: {})
+                    .labelStyle(.iconOnly)
+
+                Spacer()
+
+                Button("Back", systemImage: "backward.end", action: {})
+                    .labelStyle(.iconOnly)
+
+                Spacer()
+
+                Button("Play", systemImage: "play.fill", action: {})
+                    .labelStyle(.iconOnly)
+                    .font(.largeTitle)
+
+                Spacer()
+
+                Button("Next", systemImage: "forward.end", action: {})
+                    .labelStyle(.iconOnly)
+
+                Spacer()
+
+                Button("Repeat", systemImage: "repeat", action: {})
+                    .labelStyle(.iconOnly)
+            }
         }
         .padding()
     }
+    
 }
 
 #Preview {
-    ContentView()
+    ContentView(album: .speakNow)
 }
